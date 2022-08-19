8254db3d1cc69828d721ead7a2f7f7341b5e31a1
Alberta Lynn Brubaker <findingalberta@gmail.com>
2022-08-18 22:28:09 -0500

    commit tree 69ef217a3fb63db0456d23b4de9ace7a4e53c95d

--- lib/course.rb
+++ lib/course.rb
@@ -1,2 +1,23 @@
+class Course
+  @@all = []
+  attr_accessor :title, :schedule, :description
+  def initialize
+    self.title = title
+    self.schedule = schedule
+    self.description = description
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def self.all= course
+    @@all << course
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+end
 
--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,10 +1,13 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
-  
+
+  attr_accessor :doc, :courses
+
   def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +18,24 @@ class Scraper
       end
     end
   end
+
+  def get_page
+    html = URI.open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    self.doc = Nokogiri::HTML(html)
+  end
+
+  def get_courses
+    self.get_page.css(".post")
+    
+  end
+
+  def make_courses
+    self.get_courses.each do |post|      
+      c = Course.new
+      c.title = post.css("h2").text
+      c.schedule = post.css("em.date").text
+      c.description = post.css("p").text
+    end
+  end
   
 end
-
-
-
